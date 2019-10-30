ace3f4729aafc6326db9bfc3812ef2605ea632e4
Iwamoto-Kenji <08030531kk>
2019-10-31 05:38:05 +0900

    投稿機能作成
    記事の投稿機能の作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,23 @@
 class EventsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def show
+  end
+
+  def create
+    # binding.pry
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: '作成出来ました'
+    end
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.permit(:name, :image, :text)
+  end
+
 end
